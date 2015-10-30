b73c2c07b8f49d685c1c3a434a41c585600a957d
Vitali Cernomschi <vcernomschi@pycoding.biz>
2015-10-30 17:04:52 +0200

    Revert "Improving documentation and support for deep-framework"

--- src/DeepNgToDo/Frontend/js/app/angular/index.js
+++ src/DeepNgToDo/Frontend/js/app/angular/index.js
@@ -3,10 +3,7 @@
 
 import moduleName from './name';
 
-import '../../../node_modules/todomvc-common/base.css!';
-import '../../../node_modules/todomvc-app-css/index.css!';
-import '../../../node_modules/todomvc-common/base.js';
-import _ from '../../../node_modules/underscore/underscore';
+import _ from '../../lib/underscore-min'
 import './module/index';
 import './controllers/index';
 import './services/index';
